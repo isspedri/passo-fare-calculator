1350e87c62c35fb1f69565ed281323d863c9237c
isspedri <ismailaj625@gmail.com>
2026-05-01 15:31:20 +0000

    Add initial fare data

---
+++ data.js
@@ -0,0 +1,70 @@
+// Initial fare data for PASSO
+const fares = [
+    {
+      id: 1,
+      from: "Banjul",
+      to: "Serekunda",
+      vehicleType: "Taxi",
+      price: 15,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 2,
+      from: "Banjul",
+      to: "Serekunda",
+      vehicleType: "Bus",
+      price: 12,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 3,
+      from: "Banjul",
+      to: "Serekunda",
+      vehicleType: "Gelegele",
+      price: 10,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 4,
+      from: "Brikama",
+      to: "Banjul",
+      vehicleType: "Taxi",
+      price: 25,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 5,
+      from: "Brikama",
+      to: "Banjul",
+      vehicleType: "Bus",
+      price: 20,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 6,
+      from: "Serekunda",
+      to: "Brikama",
+      vehicleType: "Taxi",
+      price: 20,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 7,
+      from: "Banjul",
+      to: "Soma",
+      vehicleType: "Bus",
+      price: 80,
+      lastUpdated: "2026-02-13"
+    },
+    {
+      id: 8,
+      from: "Farafenni",
+      to: "Banjul",
+      vehicleType: "Bus",
+      price: 100,
+      lastUpdated: "2026-02-13"
+    }
+  ];
+  
+  // Export for use in other files
+  module.exports = { fares };
